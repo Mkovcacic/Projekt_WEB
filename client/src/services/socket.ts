ea499cb0390e1c2e2700332829335e2242f9ab47
Marko Kovačić <marko.kovacic031@gmail.com>
2026-09-01 18:30:06 +0200

    Update SOCKET_URL to use VITE_API_URL

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-const SOCKET_URL = import.meta.env.SERVER_API_URL
+const SOCKET_URL = import.meta.env.VITE_API_URL
 
 export const socket = io(SOCKET_URL, { autoConnect: false, })
